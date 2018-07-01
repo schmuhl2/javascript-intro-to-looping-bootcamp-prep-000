52ede00dc49daefe901e851a0d0fcc43cc8975cb
Stephen Schmuhl <schmuhl2@outlook.com>
2018-07-01 16:22:19 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,8 +1,11 @@
-for (var i = 1; i < 100; i++) {
-  console.log( "Hello World the " + i + " time" );
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+      array.push("I am 1 strange loop.")
+    } else {
+      array.push(`I am ${i} strange loops.`)
+    }
+  }
+
+  return array
 }
-function ForLoop(array) {
-  for(let i = 0; i < 25;
-  i++)
-}
-return string()
