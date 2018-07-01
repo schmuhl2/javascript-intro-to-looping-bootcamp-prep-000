b467c8153081b29ccb54ea5863aee707f2578a50
Stephen Schmuhl <schmuhl2@outlook.com>
2018-07-01 16:26:34 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -17,3 +17,15 @@ function whileLoop(n) {
 
   return 'done'
 }
+
+function doWhileLoop(array) {
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+
+  do {
+    array = array.slice(1)
+  } while (array.length > 0 && maybeTrue())
+
+  return array
+}
